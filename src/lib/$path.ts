96d729b636185f6c810c49f0e9251ecf656d6498
t-jimbo <jbear.um@gmail.com>
2023-04-16 12:49:46 +0900

    chore: prettier ignore

--- src/lib/$path.ts
+++ src/lib/$path.ts
@@ -1,8 +1,8 @@
 export const pagesPath = {
-  $url: (url?: { hash?: string }) => ({
-    pathname: "/" as const,
-    hash: url?.hash,
-  }),
-};
+  "todos": {
+    $url: (url?: { hash?: string }) => ({ pathname: '/todos' as const, hash: url?.hash })
+  },
+  $url: (url?: { hash?: string }) => ({ pathname: '/' as const, hash: url?.hash })
+}
 
-export type PagesPath = typeof pagesPath;
+export type PagesPath = typeof pagesPath
